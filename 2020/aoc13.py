82ac6a8310215c6e8e0fb2a8ac07c7d466193c46
Martyn <martyn.smith560@outlook.com>
2020-12-13 13:12:31 +0000

    attempted CRT but no integration of variable x

--- 2020/aoc13.py
+++ 2020/aoc13.py
@@ -1,4 +1,5 @@
 from math import prod
+from itertools import permutations
 
 with open("13.txt") as f:
     arrival = int(f.readline())
@@ -18,19 +19,21 @@ def get_timestamp():
     trial_time = 0
     while True:
         w = [w[1] for w in get_wait_times(trial_time)]
-        #print(w, ids)
-        #print(trial_time)
         if w == ids:
             return trial_time
         trial_time += 1
 
-
+#part 1
 w = min(get_wait_times(arrival), key= lambda x: x[1])
 print(w[0] * w[1])
 
-#<1748774091859561
-for b in buses:
-    for c in buses:
-        if b % c == 0 and b != c:
-            print(f"{b} and {c} are not coprime")
-print(prod(b for b in buses))
+if any(b % c == 0 and b != c for b, c in permutations(buses, 2)):
+    print("not coprime. Quitting...")
+    exit(1)
+
+#<923468884334109
+B = prod(b for b in buses) # == 1748774091859561, higher than true answer
+y = [B // b for b in buses]
+z = [pow(i, -1, b) for i, b in zip(y, buses)]
+print(B)
+print(sum(i*j*k for i, j, k in zip(ids, y, z)) % B)
