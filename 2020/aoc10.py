7c5664ad917ede4c4fb99fe2d9f76bcdd217a261
Martyn <martyn.smith560@outlook.com>
2020-12-13 14:31:48 +0000

    applied functools caching to 10 optimised 13

--- 2020/aoc10.py
+++ 2020/aoc10.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 with open("10.txt") as f:
     lines = f.readlines()
 
@@ -9,20 +10,27 @@ jolts.append(max(jolts) + 3)
 deltas = [i - j for i, j in zip(jolts[1:], jolts[:-1])]
 print(deltas.count(1) * deltas.count(3))
 
-paths = {}
-def find_valid_paths(jolts):
-    if len(jolts) == 1: #endpoint
-        paths[jolts[-1]] = 1
-        return 1
-    #print(f"no precache found")
-    count = 0
-    for i in range(3):
-        if jolts[0] + i + 1 in paths: #precache test
-            count += paths[jolts[0] + i + 1]
-        elif jolts[0] + i + 1 in jolts:
-            count += find_valid_paths(jolts[jolts.index(jolts[0] + i + 1):])
-    if count > 0:
-        paths[jolts[0]] = count
-    return count
+# paths = {}
+# def find_valid_paths(jolts):
+#     if len(jolts) == 1: #endpoint
+#         paths[jolts[-1]] = 1
+#         return 1
+#     #print(f"no precache found")
+#     count = 0
+#     for i in range(3):
+#         if jolts[0] + i + 1 in paths: #precache test
+#             count += paths[jolts[0] + i + 1]
+#         elif jolts[0] + i + 1 in jolts:
+#             count += find_valid_paths(jolts[jolts.index(jolts[0] + i + 1):])
+#     if count > 0:
+#         paths[jolts[0]] = count
+#     return count
 
-print(find_valid_paths(jolts))
+@lru_cache(None)
+def new_find_valid_paths(jolt):
+    if jolt == jolts[-1]:
+        return 1
+    return sum(new_find_valid_paths(jolt + i + 1) for i in range(3)
+                   if jolt + i + 1 in jolts)
+
+print(new_find_valid_paths(jolts[0]))
--- 2020/aoc13.py
+++ 2020/aoc13.py
@@ -7,21 +7,7 @@ with open("13.txt") as f:
     ids, buses = [b[0] for b in buses], [b[1] for b in buses]
 
 def get_wait_times(arrival):
-    wait_times = []
-    for bus in buses:
-        i = 0
-        while bus * i < arrival:
-            i+=1
-        wait_times.append((bus, (bus*i) - arrival))
-    return wait_times
-
-def get_timestamp():
-    trial_time = 0
-    while True:
-        w = [w[1] for w in get_wait_times(trial_time)]
-        if w == ids:
-            return trial_time
-        trial_time += 1
+    return [bus - arrival % bus for bus in buses]
 
 def chinese_remainder():
     times = [b - i for b, i in zip(buses, ids)]
@@ -31,12 +17,12 @@ def chinese_remainder():
     return sum(prod(x) for x in zip(times, y, z)) % B
 
 #part 1
-w = min(get_wait_times(arrival), key= lambda x: x[1])
+w = min(zip(buses, get_wait_times(arrival)), key = lambda x: x[1])
 print(w[0] * w[1])
 
 #part 2
 if any(b % c == 0 and b != c for b, c in permutations(buses, 2)):
-    print("not coprime. Quitting...")
+    print("not coprime! Quitting...")
     exit(1)
 
 print(chinese_remainder())
