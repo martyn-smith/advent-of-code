763eeb6f7e774e790603f753c289ffca052ef089
Martyn <martyn.smith560@outlook.com>
2020-12-17 13:24:36 +0000

    started day 17, fixed day 4 at last.

---
+++ 2020/aoc17.py
@@ -0,0 +1,87 @@
+"""
+Advent of code day 17: solving the world's energy crisis with Conway.
+During a cycle, all cubes simultaneously change their state according to the following rules:
+"""
+
+from itertools import product
+
+class ConwayCube:
+    def __init__(self):
+        with open("17.test.txt") as f:
+            lines = f.readlines()
+        self.cube =  [[line.strip("\n") for line in lines]]
+        self.dimensions = {"x": len(self.cube[0][0]), "y": len(self.cube[0]), "z": len(self.cube)}
+        #print(self.cube[0])
+        #print(self.dimensions) #x, y, z
+
+    def __repr__(self):
+        ret = "" 
+        for z, slice_ in enumerate(self.cube):
+            ret += f"z = {z}\n"
+            ret += "\n".join([row for row in slice_])
+            ret += "\n\n"
+        return ret
+
+    def new_slice(self):
+        return ["." * (self.dimensions["x"] + 2)] * (self.dimensions["y"] + 2)
+
+    def expand_slice(self, slice_):
+        return ["." * (self.dimensions["x"] + 2)] + [("." + s + ".") for s in slice_] + ["." * (self.dimensions["x"] + 2)]
+
+    def step(self):
+        #generate the new cube as the old one - then ignore the extremities when iterating as we know they're empty.
+        new_cube = [self.new_slice()]
+        for slice_ in self.cube:
+            new_cube.append(self.expand_slice(slice_))
+        new_cube.append(self.new_slice())
+       # print(new_cube)
+        to_activate = []
+        to_deactivate = []
+        
+        for z in range(self.dimensions["z"]+2): #0 to old_max_z
+            for y in range(self.dimensions["y"]+2):
+                for x in range(self.dimensions["x"]+2):
+                    """
+                    If a cube is active and exactly 2 or 3 of its neighbors are also active, the cube remains active. Otherwise, the cube becomes inactive.
+                    If a cube is inactive but exactly 3 of its neighbors are active, the cube becomes active. Otherwise, the cube remains inactive.
+                    """
+                    active_count = sum(1 for neighbours in product((x-1, x, x+1), (y-1, y, y+1), (z-1, z, z+1))
+                                          if not all((neighbours[0] == x, 
+                                                      neighbours[1] == y, 
+                                                      neighbours[2] == z))
+                                          and not any((neighbours[0] not in range(self.dimensions["x"] + 2),
+                                                       neighbours[1] not in range(self.dimensions["y"] + 2),
+                                                       neighbours[2] not in range(self.dimensions["z"] + 2)))
+                                          and new_cube[neighbours[2]][neighbours[1]][neighbours[0]] == "#")
+                    if active_count == 3 and new_cube[z][y][x] == ".":
+                        to_activate.append((x,y,z))
+                    elif (active_count < 1 or active_count > 3) and new_cube[z][y][x] == "#":
+                        to_deactivate.append((x,y,z)) 
+        for a in to_activate:
+            new_cube[a[2]][a[1]] = new_cube[a[2]][a[1]][:a[0]] + "#" + new_cube[a[2]][a[1]][a[0]+1:]
+        for d in to_deactivate:
+            new_cube[a[2]][a[1]] = new_cube[a[2]][a[1]][:a[0]] + "." + new_cube[a[2]][a[1]][a[0]+1:]
+        self.cube = new_cube
+        self.dimensions["x"] += 2
+        self.dimensions["y"] += 2
+        self.dimensions["z"] += 2
+
+    def count_actives(self):
+        active_count = 0
+        for slice_ in self.cube:
+            for row in slice_:
+                for cell in row:
+                    if cell == "#":
+                        active_count += 1
+        return active_count
+
+#<347
+c = ConwayCube()
+print(c)
+for i in range(1):
+    c.step()
+    print(f"after {i}:")
+    print(c)
+print(c.count_actives())
+
+#240, 1180
--- 2020/aoc4.py
+++ 2020/aoc4.py
@@ -2,25 +2,24 @@
 Advent of code day 4: "assisting" passport validation.
 """
 import re
-re.DOTALL = True
+#re.DOTALL = True
 
 #validations
 # byr (Birth Year)
-byr = re.compile(".*byr:(\d{4}).*") #1920 - 2002
+byr = re.compile(".*byr:(\d{4})") #1920 - 2002
 # iyr (Issue Year)
-iyr = re.compile(".*iyr:(\d{4}).*") #2020 - 2020
+iyr = re.compile(".*iyr:(\d{4})") #2020 - 2020
 # eyr (Expiration Year)
-eyr = re.compile(".*eyr:(\d{4}).*") #2020 - 2030
+eyr = re.compile(".*eyr:(\d{4})") #2020 - 2030
 # hgt (Height)
-hgt = re.compile(".*hgt:(\d+)(cm|in).*") #150-193 cm, 59 - 76 in
+hgt = re.compile(".*hgt:(\d+)(cm|in)") #150-193 cm, 59 - 76 in
 # hcl (Hair Color)
-hcl = re.compile(".*hcl:\#[0-9a-f]{6}.*")
+hcl = re.compile(".*hcl:\#[0-9a-f]{6}")
 # ecl (Eye Color)
-ecl = re.compile(".*ecl:(amb|blu|brn|gry|grn|hzl|oth).*")
+ecl = re.compile(".*ecl:(amb|blu|brn|gry|grn|hzl|oth)")
 # pid (Passport ID)
-pid = re.compile(".*pid:\d{9}.*")
+pid = re.compile(".*pid:\d{9}( |$)")
 # cid (Country ID) unused
-validation = [byr, iyr, eyr, hgt, hcl, ecl, pid]
 
 ranges = { 
     "birth_year": range(1920, 2002+1),
@@ -33,29 +32,55 @@ with open("4.txt") as f:
     passports = f.read().split("\n\n")
 
 def is_complete(passport: str) -> bool:
-    print("matching...")
-    for i, v in zip(["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"], validation):
-        if v.match(passport):
-            print(f"{passport} matches {i}")
-        else:
-            print(f"{passport} does not match {i}")
-    return all(v.match(passport) for v in validation)
+    """
+    Basic passport validation - just checks if field in present.
+    """
+    validation = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+    return all(v in passport for v in validation)
 
 def is_valid(passport: str) -> bool:
+    """
+    Enhanced passport validation checks.
+    """
     if not is_complete(passport):
+        #print(f"{passport}: not complete")
         return False
 
-    birth_year = int(byr.match(passport).group(1))
-    issue_year = int(iyr.match(passport).group(1))
-    expiration_year = int(eyr.match(passport).group(1))
-    height, units = int(hgt.match(passport).group(1)), hgt.match(passport).group(2)
+    if m := byr.match(passport):
+        birth_year = int(m.group(1))
+    else:
+        #print(f"{passport}: no birth year")
+        return False
+    if m := iyr.match(passport):
+        issue_year = int(m.group(1))
+    else:
+        #print(f"{passport}: no issue year")
+        return False
+    if m := eyr.match(passport):
+        expiration_year = int(m.group(1))
+    else:
+        #print(f"{passport}: no exp year")
+        return False
+    if m := hgt.match(passport):
+        height, units = int(m.group(1)), m.group(2)
+    else:
+        #print(f"{passport}: no height")
+        return False
 
     return all((
             birth_year in ranges["birth_year"],
             issue_year in ranges["issue_year"],
             expiration_year in ranges["expiration_year"],
-            height in ranges["height"][units]))
+            height in ranges["height"][units],
+            hcl.match(passport),
+            ecl.match(passport), 
+            pid.match(passport)))
 
-#279 entries, 213 initially valid, 147 secondary valid
-print(sum(1 for passport in passports[0:1] if is_complete(passport)))
-#print(sum(1 for passport in passports if is_valid(passport)))
+#setup. I don't actually know why the regex fails with intermittent newlines... but easy fix.
+passports = [passport.replace("\n", " ") for passport in passports]
+#279 entries, 213 initially valid, 147 secondary valid.
+#part 1
+print(sum(1 for passport in passports if is_complete(passport)))
+
+#part 2
+print(sum(1 for passport in passports if is_valid(passport)))
