9162e2de244fe2fa4b622fba5df89fcecb6e9eb3
Martyn <martyn.smith560@outlook.com>
2021-01-18 18:18:24 +0000

    day 9

---
+++ 2019/oxidised/src/aoc9.rs
@@ -0,0 +1,36 @@
+use super::intcode::Intcode;
+use std::fs;
+
+pub fn get_input() -> Vec<isize> {
+    let input = fs::read_to_string("../data/9.test.txt").unwrap();
+    input
+        .trim()
+        .split(',')
+        .map(|l| l.parse::<isize>().unwrap())
+        .collect::<Vec<isize>>()
+}
+
+pub fn part_1(intcodes: &Vec<isize>) -> Result<isize, isize> {
+    let mut computer = Intcode::new(intcodes);
+    let system_id = 1isize;
+    let inputs = vec![system_id];
+    let outputs = computer.run(inputs).unwrap();
+    //let outputs = run_intcode(intcodes, "1").unwrap();
+    assert!(
+        outputs.iter().filter(|&&i| i != 0).count() <= 1,
+        "ERROR: intcode returned too many non-zero status codes"
+    );
+    Ok(*outputs.last().unwrap())
+}
+
+pub fn part_2(intcodes: &Vec<isize>) -> Result<isize, isize> {
+    let mut computer = Intcode::new(intcodes);
+    let system_id = 5isize;
+    let inputs = vec![system_id];
+    let outputs = computer.run(inputs).unwrap();
+    assert!(
+        outputs.iter().filter(|&&i| i != 0).count() <= 1,
+        "ERROR: intcode returned too many non-zero status codes"
+    );
+    Ok(*outputs.last().unwrap())
+}
