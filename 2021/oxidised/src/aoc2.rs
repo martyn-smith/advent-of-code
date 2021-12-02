ee8ceb8c4b56beb21e0d365568c3799fe3a104c0
Martyn <>
2021-12-02 09:28:33 +0000

    added day 2

---
+++ 2021/oxidised/src/aoc2.rs
@@ -0,0 +1,41 @@
+fn get_vector(l: &str) -> (i32, i32) {
+    let mut l = l.split(" ");
+    let sign = match l.next().unwrap() {
+        "forward" => (1, 0),
+        "backward" => (-1, 0),
+        "up" => (0, -1),
+        "down" => (0, 1),
+        _ => unreachable!()
+    };
+    let mag = l.next().unwrap().parse::<i32>().unwrap();
+    (sign.0 * mag, sign.1 * mag)
+}
+
+fn aim(pos: (i32, i32, i32), v: (i32, i32)) -> (i32, i32, i32) {
+    match (v.0 != 0, v.1 != 0) {
+        //adjust aim
+        (false, true) => (pos.0, pos.1, pos.2 + v.1),
+        //adjust pos
+        (true, false) => (pos.0 + v.0, pos.1 + (pos.2 * v.0), pos.2),
+        _ => unreachable!()
+    }
+}
+
+pub fn get_input() -> Vec<(i32, i32)> {
+    include_str!("../../data/2.txt")
+        .lines()
+        .map(|l| get_vector(l))
+        .collect()
+}
+
+pub fn part_1(input: &Vec<(i32, i32)>) -> usize {
+    let pos = input.iter()
+                    .fold((0i32, 0i32), |pos, v| (pos.0 + v.0, pos.1 + v.1));
+    (pos.0 * pos.1) as usize
+}
+
+pub fn part_2(input: &Vec<(i32, i32)>) -> usize {
+    let pos = input.iter()
+                    .fold((0i32, 0i32, 0i32), |pos, &v| aim(pos, v));
+    (pos.0 * pos.1) as usize
+}
--- 2021/oxidised/src/tests.rs
+++ 2021/oxidised/src/tests.rs
@@ -6,3 +6,10 @@ fn test_day_1() {
     assert_eq!(1301, aoc1::part_1(&input));
     assert_eq!(1346, aoc1::part_2(&input));
 }
+
+#[test]
+fn test_day_2() {
+    let input = aoc2::get_input();
+    assert_eq!(1561344, aoc2::part_1(&input));
+    assert_eq!(1848454425, aoc2::part_2(&input));
+}
