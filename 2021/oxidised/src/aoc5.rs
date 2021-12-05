91d9f39c8d2772fbaa14928ff501b103c3fd9cac
Martyn <>
2021-12-05 19:37:49 +0000

    cleanup

--- 2021/oxidised/src/aoc5.rs
+++ 2021/oxidised/src/aoc5.rs
@@ -45,9 +45,9 @@ impl Line {
     }
 
     fn draw(&self, once: &mut HashSet<(usize, usize)>, twice: &mut HashSet<(usize, usize)>) {
-        //let pts =
         match self.direction() {
             Direction::Horizontal => {
+                //let pts = (self.x.0..=self.x.1).zip(std::iter::repeat(self.y.0));
                 let x_rng = self.x.0..=self.x.1;
                 let y = self.y.0;
                 for x in x_rng {
@@ -57,10 +57,10 @@ impl Line {
                 }
             },
             Direction::Vertical => {
+                //let pts = (self.x.0..=self.x.1).rev().zip(std::iter::repeat(self.y.0));
                 let y_rng = if self.y.1 < self.y.0 {self.y.1..=self.y.0} else {self.y.0..=self.y.1};
                 let x = self.x.0;
                 for y in y_rng {
-                    let x = self.x.0;
                     if !once.insert((x, y)) {
                         twice.insert((x, y));
                     }
@@ -80,48 +80,17 @@ impl Line {
                     }
                 }
             }
-        }
-/*
-        for (x, y) in pts {
-            if !once.insert((x,y)) {
-                println!("({}, {}) exist", x, y);
-                twice.insert((x,y));
-            }
-        }
-*/
+        };
+
+        // for (x, y) in pts {
+        //     if !once.insert((x,y)) {
+        //         println!("({}, {}) exist", x, y);
+        //         twice.insert((x,y));
+        //     }
+        // }
+
     }
 
-}
-
-/*
-fn intersects(i: usize, j: usize, input: &Vec<Line>) -> bool {
-    let (p1, p2) = (&input[i], &input[j]);
-    println!("comparing {:?}, {:?}", p1, p2);
-    match (p1.direction(), p2.direction()) {
-        (Direction::Diagonal, _) | (_, Direction::Diagonal) => false,
-        (Direction::Horizontal, Direction::Horizontal) => {
-            p1.y.0 == p2.y.0 && ((p1.x.0 < p2.x.0 && p1.x.1 > p2.x.0) || (p1.x.0 < p2.x.1 && p1.x.1 > p2.x.1))
-        },
-        (Direction::Vertical, Direction::Vertical) => {
-            p1.x.0 == p2.x.0 && ((p1.y.0 < p2.y.0 && p1.y.1 > p2.y.0) || (p1.y.0 < p2.y.1 && p1.y.1 > p2.y.1))
-        },
-        (Direction::Horizontal, Direction::Vertical) => {
-            (p1.x.0 < p2.x.0 && p1.x.1 > p2.x.0) && (p2.y.0 < p1.x.0 && p2.y.1 > p1.y.0)
-        }
-        (Direction::Vertical, Direction::Horizontal) => {
-            (p1.y.0 < p2.y.0 && p1.y.1 > p2.y.0) && (p2.x.0 < p1.x.0 && p2.x.1 > p1.x.0)
-        },
-    }
-}
-*/
-
-fn matches(p: (usize, usize), input: &Vec<Line>) -> usize {
-    input.iter()
-        .filter(|l| l.direction() != Direction::Diagonal)
-        .filter(|l| {
-            l.x.0 <= p.0 && l.x.1 >= p.0 && l.y.0 <= p.1 && l.y.1 >= p.1
-        })
-        .count()
 }
 
 pub fn get_input() -> Vec<Line> {
@@ -132,17 +101,6 @@ pub fn get_input() -> Vec<Line> {
 }
 
 pub fn part_1(input: &Vec<Line>) -> usize {
-
-    let max_x = input.iter()
-                     .map(|l| l.x.1)
-                     .max()
-                     .unwrap();
-
-    let max_y = input.iter()
-                     .map(|l| l.y.1)
-                     .max()
-                     .unwrap();
-
     let (mut once, mut twice) = (HashSet::new(), HashSet::new());
     let lines = input.iter().filter(|&l| l.direction() != Direction::Diagonal);
     for l in lines {
@@ -152,16 +110,6 @@ pub fn part_1(input: &Vec<Line>) -> usize {
 }
 
 pub fn part_2(input: &Vec<Line>) -> usize {
-    let max_x = input.iter()
-                     .map(|l| l.x.1)
-                     .max()
-                     .unwrap();
-
-    let max_y = input.iter()
-                     .map(|l| l.y.1)
-                     .max()
-                     .unwrap();
-
     let (mut once, mut twice) = (HashSet::new(), HashSet::new());
     let lines = input.iter();
     for l in lines {
